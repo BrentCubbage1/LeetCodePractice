36faf57ab65fab8f68a9f8678942e4879a0e8ad8
Brent <BrentC0328@gmail.com>
2023-10-16 17:12:10 -0400

    more work on ContinousArray

--- src/main/java/Questions/ContinousArray/ContinousArray.java
+++ src/main/java/Questions/ContinousArray/ContinousArray.java
@@ -17,24 +17,38 @@ public class ContinousArray {
         //element = nums.length - 1. + minumum - count;
         //I'm not sure if this covers all edge cases, but I'll start here. */
 
+        //Didn't need a treeset because I can just sort an Array with Arrays.sort or Collections.sort a list.
+        //Ok now I added a TreeSet to get the difference in size between the array and the set and add it to the total
+        //This is intended to cover multiples that would need to be changed.
+
+
 
         ArrayList<Integer> sortedArray = new ArrayList<>();
-        for (int num : nums){
-            sortedArray.add(num);
-        }
-
-        Collections.sort(sortedArray);
         int count = 0;
-        Integer minimum = sortedArray.get(0);
-        Integer maximum = sortedArray.get(nums.length - 1);
 
-        for (int i = nums.length - 1; i > 0; i--){
-            Integer check = sortedArray.get(i);
-            if (check - minimum > nums.length - 1){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++){
+            int current = nums[i];
+            int next = nums[i + 1];
+
+            if (current == next){
+                nums[i+1] = nums[i+1] +1;
+                sortedArray.add(current);
+                count++;
+            } else {
+                sortedArray.add(current);
+            }
+        }
+        sortedArray.add(nums[nums.length - 1]);
+
+        Integer minimum = sortedArray.get(0);
+
+        for (int num : sortedArray){
+            if (num - minimum > nums.length - 1){
                 count++;
             }
         }
-        return count;
+        return count ;
 
 
 
