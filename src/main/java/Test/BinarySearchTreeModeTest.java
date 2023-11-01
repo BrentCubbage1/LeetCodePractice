05a1f5f5453412c06f6262a12f8dc04b35f3935b
Brent <BrentC0328@gmail.com>
2023-11-01 19:11:52 -0400

    Added second test to BinarySearchTreeMode for TDD

--- src/main/java/Test/BinarySearchTreeModeTest.java
+++ src/main/java/Test/BinarySearchTreeModeTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertArrayEquals;
 public class BinarySearchTreeModeTest {
 
     @Test
-    public void testFindMode() {
+    public void testFindModeTest() {
         TreeNode root = new TreeNode(1, new TreeNode(2), new TreeNode(2));
 
         BinarySearchTreeMode finder = new BinarySearchTreeMode();
@@ -20,6 +20,21 @@ public class BinarySearchTreeModeTest {
         int[] expected = new int[1];
         expected[0] = 2;
 
+        Assert.assertEquals(expected[0], actual[0]);
+    }
+
+    @Test
+    public void testFindModeTest2() {
+        TreeNode root = new TreeNode(4,new TreeNode(2),new TreeNode(6) );
+        root.getLeft().setLeft(new TreeNode(2));
+        root.getLeft().setRight(new TreeNode(3));
+        root.getRight().setLeft(new TreeNode(5));
+        root.getRight().setRight( new TreeNode(7));
+
+        BinarySearchTreeMode finder = new BinarySearchTreeMode();
+        int[] actual = finder.findMode(root);
+
+        int[] expected = {2};
         Assert.assertEquals(expected[0], actual[0]);
     }
 }
