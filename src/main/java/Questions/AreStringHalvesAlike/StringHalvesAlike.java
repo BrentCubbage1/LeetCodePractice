f6e52f821afe2656d6a09c36e858c0aaa27554e9
Brent <BrentC0328@gmail.com>
2024-01-11 19:37:05 -0500

    leetcode daily 1/11/24 Are String Halves Alike

---
+++ src/main/java/Questions/AreStringHalvesAlike/StringHalvesAlike.java
@@ -0,0 +1,43 @@
+package Questions.AreStringHalvesAlike;
+
+public class StringHalvesAlike {
+
+    public boolean halvesAreAlike(String s) {
+
+        //1. We need the first and second half of the string.
+        //2. Then we count through the two strings.
+        //3. Remember that vowels can be lowercase or uppercase.
+        //4. if each letter is a vowel, add to counter.
+        //5. return aCount == bCount.
+
+        //1.
+        int split = s.length() / 2;
+
+        String a = s.substring(0, split);
+        String b = s.substring(split, s.length());
+        int aCount = 0;
+        int bCount = 0;
+
+        //3.
+        List<Character> vowels = new ArrayList<>(Arrays.asList('a','e','i','o','u', 'A','E', 'I','O','U'));
+
+        //2.
+        for (int i = 0; i < a.length(); i++){
+            Character charA = a.charAt(i);
+            Character charB = b.charAt(i);
+
+            //4.
+            if(vowels.contains(charA)){
+                aCount++;
+            }
+
+            if(vowels.contains(charB)){
+                bCount++;
+            }
+        }
+
+        //5.
+        return aCount == bCount;
+
+    }
+}
